c5966e3a4432667c508598e641022c2f3b874976
Simon <silvestresimon@icloud.com>
2020-11-24 18:20:27 +0100

    Domotique video

--- src/views/Alarme.vue
+++ src/views/Alarme.vue
@@ -45,7 +45,7 @@
           </div>
         </div>
       </div>
-        <a href="#slide2">
+        <a href="#video_alarme">
           <svg class="scrollDown" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 512.008 512.008" style="enable-background:new 0 0 512 512" xml:space="preserve"><g>
             <path xmlns="http://www.w3.org/2000/svg" style="" d="M306.219,45.796c-21.838-21.838-57.245-21.838-79.083,0s-21.838,57.245,0,79.083l77.781,77.803  H53.333C23.878,202.682,0,226.56,0,256.015c0,29.455,23.878,53.333,53.333,53.333h251.584l-77.781,77.781  c-21.838,21.838-21.838,57.245,0,79.083c21.838,21.838,57.245,21.838,79.083,0l202.667-202.667c4.164-4.165,4.164-10.917,0-15.083  L306.219,45.796z" fill="#ffffff" data-original="#ff9800" class=""/>
             <path xmlns="http://www.w3.org/2000/svg" d="M266.667,482.575c-30.869,0.033-55.92-24.965-55.952-55.834c-0.016-14.859,5.886-29.112,16.4-39.611l77.803-77.781H53.333  C23.878,309.348,0,285.47,0,256.015c0-29.455,23.878-53.333,53.333-53.333h251.584L227.115,124.9  c-21.838-21.838-21.838-57.245,0-79.083s57.245-21.838,79.083,0l202.667,202.667c4.164,4.165,4.164,10.917,0,15.083L306.197,466.234  C295.716,476.719,281.492,482.599,266.667,482.575z M53.333,224.015c-17.673,0-32,14.327-32,32s14.327,32,32,32h277.333  c5.891-0.011,10.675,4.757,10.686,10.648c0.005,2.84-1.123,5.565-3.134,7.571l-96,96c-13.508,13.508-13.508,35.409,0,48.917  c13.508,13.508,35.409,13.508,48.917,0l195.115-195.136L291.115,60.9c-13.508-13.508-35.409-13.508-48.917,0  c-13.508,13.508-13.508,35.409,0,48.917l0,0l96,96c4.171,4.16,4.179,10.914,0.019,15.085c-2.006,2.011-4.731,3.139-7.571,3.134  L53.333,224.015z" fill="#1faf8e" data-original="#000000" style="" class=""/>
@@ -83,7 +83,7 @@
           </svg>
         </a>
     </div>
-    <div id="slide2">
+    <div id="video_alarme">
       <iframe src="https://www.youtube.com/embed/RRbvzVAwUr4" allowfullscreen></iframe>
     </div>
   </div>
@@ -196,8 +196,7 @@ export default {
   .scrollDown:hover path {
     fill: #1faf8e;
   }
-
-  #slide2 {
+  #video_alarme {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -207,7 +206,7 @@ export default {
     font-weight: bold;
     color: #fff;
   }
-  #slide2 iframe {
+  #video_alarme iframe {
     width: 80% ;
     height: 80%;
     border: none;
--- src/views/Domotique.vue
+++ src/views/Domotique.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="domotique">
-
+     <div id="video_domotique">
+      <iframe src="https://www.youtube.com/embed/T1vsZjZcavk" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
 
@@ -10,12 +12,26 @@
 
 export default {
   name: 'Domotique',
-  components: {
-    
-  }
 }
 </script>
 
 <style scoped>
-
+  .domotique {
+    margin-top: 80px;
+  }
+  #video_domotique {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 80px); 
+    background-color: #1faf8e;
+    font-size: 100px;
+    font-weight: bold;
+    color: #fff;
+  }
+  #video_domotique iframe {
+    width: 80% ;
+    height: 80%;
+    border: none;
+  }
 </style>
